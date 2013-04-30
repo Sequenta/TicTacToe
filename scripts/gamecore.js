2217be76335394ff3a60226b2d5aba147156cbf0
Axel <tranquilized.mind@gmail.com>
2013-04-30 19:16:28 +0500

    Added Real implementation of drawField function

--- scripts/gamecore.js
+++ scripts/gamecore.js
@@ -1,4 +1,28 @@
 function drawField(width,heigth) {
-    alert("Field parameters:\n width " + width +"\n heigth:"+ heigth);
+
+    var cellWidth = 20;
+    var cellHeight = 20
+    var columns =  width;
+    var rows = heigth;
+
+    var canvasWidth = cellWidth * columns;
+    var canvasHeight = cellHeight * rows;
+
+    var canvas = document.getElementById('gameGrid');
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    var ctx = canvas.getContext('2d');
+    drawGrid(ctx);
+
+    function drawGrid(ctx) {
+        ctx.beginPath();
+        for (i = 0; i < columns; i++) {
+            for (j = 0; j < rows; j++) {
+                ctx.strokeRect(cellWidth * i, cellHeight * j, cellWidth, cellHeight);
+            }
+        }
+        ctx.closePath();
+    }
+    alert("Field parameters:\n width " + width + "\n heigth:" + heigth);
 }
 
